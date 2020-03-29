572dc5c3c75974c58723a21acbe4ce114e1c64dd
yaltha <yaltharullist@gmail.com>
2020-03-30 04:16:56 +0700

    truthy source

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './components/counters';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
