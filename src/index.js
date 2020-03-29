8007e8036c1b3dd87c3f4486933b910a98a0e986
yaltha <yaltharullist@gmail.com>
2020-03-30 05:01:08 +0700

    multiple comments in sync

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Counters />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
