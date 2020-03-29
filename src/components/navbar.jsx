263f23416a5892054ef277e43a2d43c954d6c8a5
yaltha <yaltharullist@gmail.com>
2020-03-30 06:17:11 +0700

    sfc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar {" "}
-                    <span 
-                    className="badge badge-pill badge-secondary">
-                    {this.props.totalCounter}
-                    </span>
-                </a>
-            </nav> 
-        );
-    }
+//Stateles FUnctional Component
+
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+            Navbar {" "}
+            <span 
+                className="badge badge-pill badge-secondary">
+            {props.totalCounter}
+            </span>
+        </a>
+</nav>  );
 }
  
 export default NavBar;
+
